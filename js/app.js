442016b0e43003aad5f2b71c6b1302d8aab3a0f7
Nikita Belevich <belevish_1998@mail.ru>
2020-05-26 12:23:22 +0300

    Задача 2 (Улучшенная подсказка)

--- js/app.js
+++ js/app.js
@@ -23,3 +23,35 @@ function selectFile(event) {
     }
 }
 //TODO Task 1
+
+//TODO Task 2
+const housePigs = document.querySelector('.task2 .house-pigs');
+housePigs.addEventListener('mouseover', showTooltip);
+
+function showTooltip(event) {
+    const targetPath = event.target.closest('[data-tooltip]');
+    const coordinatesTargetElem = targetPath.getBoundingClientRect(); // получили координаты ближайшего элемента с атр. data-tooltip
+
+    const tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip'); // добавили стили
+    tooltip.textContent = targetPath.dataset.tooltip; // добавили текст из атрибута элемента
+    document.body.append(tooltip); // разместили
+
+    let top = coordinatesTargetElem.top - (tooltip.offsetHeight + 5); // определяем верхнюю позицию подсказки
+    if (coordinatesTargetElem.top < tooltip.offsetHeight + 5) { // если прокрутка до верхней границы меньше высоты подсказки
+        top = coordinatesTargetElem.bottom + 5; // тогда размещаем внизу элемента
+    }
+
+    let left = coordinatesTargetElem.left + (targetPath.offsetWidth - tooltip.offsetWidth) / 2; // центрируем подсказку по центру элемента
+    
+    tooltip.style.cssText = `left: ${left}px;
+                             top: ${top}px;
+                              `;
+}
+
+housePigs.addEventListener('mouseout', removeTooltip);
+function removeTooltip(event) {
+    const tooltip = document.querySelector('.tooltip');
+    tooltip.remove();
+}
+//TODO Task 2
